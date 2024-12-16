d812958d1ab664423e402146b3996f2aa1e3e9be
Elias El Khoury <90423789+Khoury-e@users.noreply.github.com>
2024-12-16 02:34:35 +0200

    Delete secure-path-backend directory

--- secure-path-backend/helpers/jwt.helper.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-
-exports.sign = (email) => {
-    return jwt.sign({email: email}, process.env.JWT_KEY, {
-        expiresIn: "1h"
-    });
-};
--- secure-path-backend/migrations/20241122150319-add-is-verified-to-users.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  async up(db, client) {
-    await db.collection('users').updateMany({}, {$set: {isVerified: false}});
-  },
-
-  async down(db, client) {
-    await db.collection('users').updateMany({}, {$unset: {isVerified: ''}})
-  }
-};
