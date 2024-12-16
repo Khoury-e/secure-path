b3357b228c723d783d30c48dd45c9c833b571086
Elias El Khoury <90423789+Khoury-e@users.noreply.github.com>
2024-12-16 02:33:56 +0200

    Delete secure-path-backend/routes/Admin directory

--- secure-path-backend/routes/Admin/admin.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const ModuleController = require("../../controllers/ModuleController");
-const authMiddleware = require("../../middlewares/auth.middlewares");
-// all routes
-router.post("/add/module", authMiddleware.isAdmin, ModuleController.addModule);
-
-module.exports = router;
