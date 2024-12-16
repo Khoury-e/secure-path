ea2d5d9abdfeaebd9eff438d8e5c3e508fcfa1e7
Elias El Khoury <90423789+Khoury-e@users.noreply.github.com>
2024-12-16 09:26:02 +0200

    Add files via upload

--- secure-path-backend/routes/Auth/auth.js
+++ secure-path-backend/routes/Auth/auth.js
@@ -6,6 +6,9 @@ const validator = require("../../helpers/validator.helper");
 // all auth routes
 router.post("/login", authController.login);
 router.post("/signup", validator.newUser, authController.signup);
+router.get("/logout", authController.logout);
 router.post("/verify-email/:userId/:token", authController.verifyEmail);
+router.get("/isLoggedIn", authController.isLoggedIn);
+router.get("/isVerified", authController.isVerifiedAccount);
 
 module.exports = router;
