f6c97682568e462558e9a669e95f4c6989cb25b1
Elias El Khoury <90423789+Khoury-e@users.noreply.github.com>
2024-12-16 09:25:39 +0200

    Add files via upload

---
+++ secure-path-backend/routes/User/user.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+const ModuleController = require("../../controllers/ModuleController");
+const UserController = require("../../controllers/UserController");
+const authMiddleware = require("../../middlewares/auth.middlewares");
+
+// all user routes
+router.get("/load-profile", authMiddleware.isAuth, UserController.loadProfile)
+router.get("/modules/all", authMiddleware.isAuth, ModuleController.getAllModules);
+router.get("/module/:title",authMiddleware.isAuth, ModuleController.getModule);
+router.get("/load-exercise/:title/:difficulty", authMiddleware.isAuth, UserController.loadExercise);
+router.post("/username-change", authMiddleware.isAuth, UserController.changeUsername);
+router.post("/fullname-change", authMiddleware.isAuth, UserController.changeFullName);
+router.post("/solve-exercise", UserController.solveExercise);
+router.get("/get-stats", authMiddleware.isAuth, UserController.getStats);
+router.get("/delete-account",authMiddleware.isAuth ,UserController.deleteAccount);
+
+module.exports = router;
